948dfb93096c03fe8423330d0b23157dcf223c98
Lexie Crawford <lexcraw4d@gmail.com>
2021-05-22 20:21:38 -0400

    added mock testing for player's potion inventory

--- _tests_/Potion.test.js
+++ _tests_/Potion.test.js
@@ -1,17 +1,19 @@
+
+
 const Potion = require('../lib/Potion.js');
 //Potion created with args:
 test('creates a health potion object', () => {
-    const potion = new Potion ('Health');
+	const potion = new Potion('Health');
 
-    expect(potion.name).toBe('Health');
-    expect(potion.value).toEqual(expect.any(Number));
+	expect(potion.name).toBe('Health');
+	expect(potion.value).toEqual(expect.any(Number));
 });
 
 //Potion without args passed test:
-test ('creates a random potion object', () => {
-    const potion = new Potion ();
+test('creates a random potion object', () => {
+	const potion = new Potion();
 
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
-    expect(potion.value).toEqual(expect.any(Number))
+	expect(potion.name).toEqual(expect.any(String));
+	expect(potion.name.length).toBeGreaterThan(0);
+	expect(potion.value).toEqual(expect.any(Number));
 });
